338ee11e8c5dd8da42e0ddbcbed3d4ad4a7b2846
spc <spc>
2005-09-19 04:57:10 +0000

    Updated the Memory subsystem to 5.0

--- src/Memory/memory.c
+++ src/Memory/memory.c
@@ -1,4 +1,3 @@
-
 /************************************************************************
 *
 * Copyright 2001 by Sean Conner.  All Rights Reserved.
@@ -31,49 +30,42 @@
 #include "../memory.h"
 #include "../ddt.h"
 #include "../util.h"
-#include "imem.h"
+
+#ifdef MIPS
+#  define PADDING	0xB1
+#  define NULLP		0xB1B1B1B1
+#elif defined __i386__
+#  define PADDING	0xCC
+#  define NULLP		0xCCCCCCCC
+#elif defined MC68K
+#  define PADDING	0xA1
+#  define NULLP		0xA1A1A1A1
+#elif defined RS6000
+#  define PADDING	0xB1
+#  define NULLP		0xB1B1B1B1
+#else
+#  error Please define system
+#endif
+
+/*#ifdef DDT*/
+#if 0
+#  define FUDGE		64
+#  define HFUDGE	(FUDGE/2)
+#  define NULLPTR(p)	((void *)(p)) == ((void *)NULLP)
+#else
+#  define FUDGE		0
+#  define HFUDGE	0
+#  define NULLPTR(p)	(p) == NULL
+#endif
 
 /**************************************************************************/
   
-static int		 mem_errhandler		(Error);
-
-/**************************************************************************/
-
 int (MemInit)(void)
 {
   int rc;
   
-  /*-----------------------------------------------------------
-  ; the order of the following operations is critical.  In fact,
-  ; a lot of the library assumes that the memory sub-system is
-  ; up and fully operational, but at this point, it isn't.  So, with
-  ; that in mind, we need to be aware of what is and isn't safe
-  ; to call.
-  ;
-  ; ErrHandlerNew() is one such safe call IN THIS CASE, since we 
-  ; are passing static memory to the call.  ErrHandlerPush() isn't
-  ; safe until we can actually allocate memory through this subsystem,
-  ; but's that okay if we can't register a handler because the default
-  ; action of an empty handler chain is to return the error.
-  ;
-  ; So we create an empty error handler chain, then create our heap,
-  ; then register a default handler (if we can).
-  ;
-  ; Theoretically, we should do the following:
-  ;
-  ;	rc = StandardCHeap(&lvheap);
-  ;	if (rc != ERR_OKAY) return(rc);
-  ;	HeapDefault(lvheap);
-  ;
-  ; But really, since this IS the implementation, and we KNOW what
-  ; HeapDefault() does (and in fact, we can see it here) we can 
-  ; just skip the HeapDefault() call.
-  ; ------------------------------------------------------------*/
-  
-  ErrHandlerNew(&MemoryErr);
   if ((rc = StandardCHeap(&HeapMain)) != ERR_OKAY)
     return(rc);
-  ErrHandlerPush(&MemoryErr,mem_errhandler);
   return(ERR_OKAY);
 }
 
@@ -85,7 +77,7 @@ Heap (HeapDefault)(const Heap heap)
   
   ddt(heap != NULL);
   
-  HeapMain     = heap;
+  HeapMain = heap;
   return(retheap);
 }
 
@@ -93,9 +85,7 @@ Heap (HeapDefault)(const Heap heap)
 
 int (HeapNew)(Heap *pheap,size_t hsize,const struct memvector *pvec,void *datum)
 {
-  Heap             heap;
-  struct memstats *pstats;
-  int              i;
+  Heap heap;
   
   ddt(pheap != NULL);
   ddt(hsize >  0);
@@ -103,18 +93,12 @@ int (HeapNew)(Heap *pheap,size_t hsize,const struct memvector *pvec,void *datum)
 
   heap = (*pvec->hnew)(hsize,datum);
   if (heap == NULL)
-    return(ErrorPush(CgiErr,HEAPNEW,ERR_NOMEMORY,""));
+    return(ERR_ERR);
     
   heap->size   = hsize;
   heap->data   = datum;
   heap->vector = pvec;
-  for (i = 0 , pstats = &heap->stats[0] ; i < STATMAX ; i++)
-  {
-    pstats[i].bytes = pstats[i].calls = pstats[i].max = 0;
-    pstats[i].min   = ULONG_MAX;
-  }
-  
-  *pheap = heap;
+  *pheap       = heap;
   return(ERR_OKAY);
 }
 
@@ -126,11 +110,6 @@ void *(HeapMemAlloc)(const Heap heap,size_t size)
   
   ddt(heap != NULL);
   ddt(size >  0);
-  
-  heap->stats[STATALLOC].bytes += size;
-  heap->stats[STATALLOC].calls ++;
-  if (size < heap->stats[STATALLOC].min) heap->stats[STATALLOC].min = size;
-  if (size > heap->stats[STATALLOC].max) heap->stats[STATALLOC].max = size;
 
   p = (*heap->vector->alloc)(heap,size + FUDGE);
   D(memset(p,PADDING,size + FUDGE);)
@@ -139,231 +118,24 @@ void *(HeapMemAlloc)(const Heap heap,size_t size)
 
 /*************************************************************************/
 
-void *(HeapMemGrow)(const Heap heap,const void *p,size_t os,size_t ns)
+void *(HeapMemResize)(const Heap heap,void *p,size_t ns)
 {
-  unsigned long delta;
-  
   ddt(heap != NULL);
-  ddt(p    != NULL);
-  ddt(os   >  0);
   ddt(ns   >  0);
-  ddt(ns   >  os);
-  
-  delta = ns - os;
-  heap->stats[STATGROW].bytes += delta;
-  heap->stats[STATGROW].calls ++;
-  if (delta < heap->stats[STATGROW].min) heap->stats[STATGROW].min = delta;
-  if (delta > heap->stats[STATGROW].max) heap->stats[STATGROW].max = delta;
 
-# ifdef DDT
-  {
-    void *np;
-    
-    np = HeapMemAlloc(heap,ns);
-    memcpy(np,p,os);
-    HeapMemFree(heap,p,os);
-    return(np);
-  }
-# else  
-    return((*heap->vector->grow)(heap,p,os,ns));
-# endif
+  return((*heap->vector->resize)(heap,(unsigned char *)(p) - HFUDGE,ns));
 }
 
 /*************************************************************************/
 
-void *(HeapMemShrink)(const Heap heap,const void *p,size_t os,size_t ns)
+void (HeapMemFree)(const Heap heap,void *p)
 {
-  unsigned long delta;
-  
   ddt(heap != NULL);
-  ddt(p    != NULL);
-  ddt(os   >  0);
-  ddt(ns   >  0);
-  ddt(ns   <= os);
-  
-  delta = os - ns;
-  heap->stats[STATSHRINK].bytes += delta;
-  heap->stats[STATSHRINK].calls ++;
-  if (delta < heap->stats[STATGROW].min) heap->stats[STATSHRINK].min = delta;
-  if (delta > heap->stats[STATGROW].max) heap->stats[STATSHRINK].max = delta;
 
-# ifdef DDT
-  {
-    void *np;
-    
-    np = HeapMemAlloc(heap,ns);
-    memcpy(np,p,ns);
-    HeapMemFree(heap,p,os);
-    return(np);
-  }
-# else
-    return((*heap->vector->grow)(heap,p,os,ns));
-# endif
+  (*heap->vector->free)(heap,(unsigned char *)(p) - HFUDGE);
 }
 
 /**************************************************************************/
-
-void *(HeapMemResize)(const Heap heap,const void *p,size_t os,size_t ns)
-{
-  ddt(heap != NULL);
-  
-  if (p != NULL)
-  {
-    if (ns == 0)
-    {
-      ddt(os > 0);
-      HeapMemFree(heap,p,os);
-      return(NULL);
-    }
-    else if (ns < os)
-      return(HeapMemShrink(heap,p,os,ns));
-    else if (ns > os)
-      return(HeapMemGrow(heap,p,os,ns));
-    else
-      ddt(0);
-  }
-  else
-    return(HeapMemAlloc(heap,ns));
-  
-  ddt(0);
-  return((void *)p);
-}
-
-/**************************************************************************/
-
-void (HeapMemFree)(const Heap heap,const void *p,size_t size)
-{
-  ddt(heap != NULL);
-  ddt(size >  0);
-  
-  heap->stats[STATFREE].bytes += size;
-  heap->stats[STATFREE].calls ++;
-  if (size < heap->stats[STATFREE].min) heap->stats[STATFREE].min = size;
-  if (size > heap->stats[STATFREE].max) heap->stats[STATFREE].max = size;
-
-# ifdef DDT
-  {
-    unsigned char *pu;
-    int            i;
-
-    for ( 
-          pu = (unsigned char *)(p) - HFUDGE , i = 0 ;
-          i < HFUDGE ;
-          pu++ , i++
-        )
-    {
-      if (*pu != PADDING)
-      {
-        ddtlog(
-                ddtstream,
-                "p i L i",
-                "%a front mem guard corrupt at %b size %c char %d",
-                (void *)p,
-                i,
-                size,
-                *pu
-              );
-        dump_memory(ddtstream->buffer,p,(size < 128) ? size : 128 , (size_t)p);
-	ddtlog(ddtstream,"","-----");
-        dump_memory(ddtstream->buffer,(unsigned char *)(p) - HFUDGE , HFUDGE , (size_t)(p) - HFUDGE);
-        ddt(*pu == PADDING);
-      }
-    }
-
-    for (
-          pu = (unsigned char *)(p) + size, i = 0 ;
-          i < HFUDGE ;
-          pu++ , i++
-        )
-    {
-      if (*pu != PADDING)
-      {
-        ddtlog(
-                ddtstream,
-                "p i L i",
-                "%a back mem guard corrupt at %b size %c char %d",
-                (void *)p,
-                i,
-                size,
-                *pu
-              );
-        dump_memory(ddtstream->buffer,p,(size < 128) ? size : 128 , (size_t)p);
-        dump_memory(ddtstream->buffer,(unsigned char *)(p) + size, HFUDGE , (size_t)(p) + size);
-        ddt(*pu == PADDING);
-      }
-    }
-
-    pu = (unsigned char *)(p) - HFUDGE;	/* fill memory with garbage */
-    memset(pu,PADDING,size+FUDGE);
-    p     = pu;
-    size += FUDGE;
-    /*(*heap->vector->free)(heap,pu,size+FUDGE);*/
-  }
-# endif
-  (*heap->vector->free)(heap,p,size);
-}
-
-/**************************************************************************/
-
-size_t (HeapSpaceTotal)(const Heap heap)
-{
-  ddt(heap != NULL);
-  return((*heap->vector->htspace)(heap));
-}
-
-/************************************************************************/
-
-size_t (HeapSpaceFree)(const Heap heap)
-{
-  ddt(heap != NULL);
-  return((*heap->vector->hfspace)(heap));
-}
-
-/************************************************************************/
-
-size_t (HeapSpaceUsed)(const Heap heap)
-{
-  ddt(heap != NULL);
-  return((*heap->vector->huspace)(heap));
-}
-
-/************************************************************************/
-
-int (HeapMemStats)(const Heap heap,struct memstats *pstats,size_t n,int calls)
-{
-  int i;
-  /*int mask;*/
-  
-  ddt(pstats          != NULL);
-  ddt(n               >  0);
-  ddt(n               <= 5);
-  ddt((calls & C_MAX) == 0);
-
-#if 1
-  for (i = 0  ; i < (int)n ; i++)
-  {
-    pstats[i].bytes = heap->stats[i].bytes;
-    pstats[i].calls = heap->stats[i].calls;
-    pstats[i].min   = heap->stats[i].min;
-    pstats[i].max   = heap->stats[i].max;
-  }
-#else
-  for (mask = 0 , i = 0 ; (mask < 5) && (i < n) ; mask++)
-  {
-    if ((calls & (1 << mask)) == (1 << mask)) 
-    {
-      pstats[i].bytes = heap->stats[mask].bytes;
-      pstats[i].calls = heap->stats[mask].calls;
-      pstats[i].min   = heap->stats[mask].min;
-      pstats[i].max   = heap->stats[mask].max;
-      i++;
-    }
-  }
-#endif
-  return(i);
-}
-
-/***************************************************************************/
 
 void (HeapFree)(Heap *pheap)
 {
@@ -388,60 +160,19 @@ void *(MemAlloc)(size_t size)
 
 /************************************************************************/
 
-void *(MemGrow)(const void *p,size_t os,size_t ns)
+void *(MemResize)(void *p,size_t ns)
 {
-  ddt(p  != NULL);
-  ddt(os >  0);
-  ddt(ns >  0);
-  ddt(os <  ns);
-        
-  return(HeapMemGrow(HeapMain,p,os,ns));
+  return(HeapMemResize(HeapMain,p,ns));
 }
 
 /*************************************************************************/
 
-void *(MemShrink)(const void *p,size_t os,size_t ns)
+void (MemFree)(void *p)
 {
-  ddt(p  != NULL);
-  ddt(os >  0);
-  ddt(ns >  0);
-  ddt(os >= ns);
+  ddt(p != NULL);
   
-  return(HeapMemShrink(HeapMain,p,os,ns));
-}
-
-/***************************************************************************/
-
-void *(MemResize)(const void *p,size_t os,size_t ns)
-{
-  return(HeapMemResize(HeapMain,p,os,ns));
+  HeapMemFree(HeapMain,p);
 }
 
 /*************************************************************************/
-
-void (MemFree)(const void *p,size_t size)
-{
-  ddt(p    != NULL);
-  ddt(size >  0);
-  
-  HeapMemFree(HeapMain,p,size);
-}
-
-/*************************************************************************/
-
-int (MemStats)(struct memstats *pstat,size_t num,int flags)
-{
-  ddt(pstat != NULL);
-  ddt(num   >  0);
-  
-  return(HeapMemStats(HeapMain,pstat,num,flags));
-}
-
-/**********************************************************************/
-
-static int mem_errhandler(Error error)
-{
-  ddtlog(ddtstream,"","out of memory");
-  return(ABORT);
-}
 
--- src/Memory/memstdc.c
+++ src/Memory/memstdc.c
@@ -1,4 +1,3 @@
-
 /************************************************************************
 *
 * Copyright 2001 by Sean Conner.  All Rights Reserved.
@@ -27,38 +26,25 @@
 
 #include "../types.h"
 #include "../nodelist.h"
-#include "../errors.h"
 #include "../memory.h"
 #include "../ddt.h"
-#include "imem.h"
 
 /*************************************************************************/
 
 static void		*heap_new	(size_t,void *);
-static size_t		 heap_tspace	(const struct heap *);
-static size_t		 heap_fspace	(const struct heap *);
-static size_t		 heap_uspace	(const struct heap *);
 static void		 heap_free	(const struct heap *);
-
 static void		*mem_alloc	(const struct heap *,size_t);
-static void		*mem_grow	(const struct heap *,const void *,size_t,size_t);
-static void		*mem_shrink	(const struct heap *,const void *,size_t,size_t);
-static void		 mem_free	(const struct heap *,const void *,size_t);
-
-static void		*mem_resize	(const struct heap *,const void *,size_t,size_t);
+static void		*mem_resize	(const struct heap *,void *,size_t);
+static void		 mem_free	(const struct heap *,void *);
 
 /**************************************************************************/
 
 static const struct memvector vector = 
 {
   heap_new,
-  heap_tspace,
-  heap_fspace,
-  heap_uspace,
   heap_free,
   mem_alloc,
-  mem_grow,
-  mem_shrink,
+  mem_resize,
   mem_free
 };
 
@@ -83,41 +69,6 @@ static void *heap_new(size_t hsize,void *data)
 
 /**********************************************************************/
 
-static size_t heap_tspace(const struct heap *pheap)
-{
-  ddt(pheap != NULL);
-  return(ULONG_MAX);
-}
-
-/***********************************************************************/
-
-static size_t heap_fspace(const struct heap *pheap)
-{
-  ddt(pheap != NULL);
-  return(
-          ULONG_MAX 
-          - pheap->stats[STATALLOC].bytes 
-          - pheap->stats[STATGROW].bytes
-          + pheap->stats[STATSHRINK].bytes
-          + pheap->stats[STATFREE].bytes
-        );
-}
-
-/************************************************************************/
-
-static size_t heap_uspace(const struct heap *pheap)
-{
-  ddt(pheap != NULL);
-  return(
-          pheap->stats[STATALLOC].bytes
-          + pheap->stats[STATGROW].bytes
-          - pheap->stats[STATSHRINK].bytes
-          - pheap->stats[STATFREE].bytes
-        );
-}
-
-/**************************************************************************/
-
 static void heap_free(const struct heap *pheap)
 {
   free((const Heap)pheap);
@@ -128,84 +79,41 @@ static void heap_free(const struct heap *pheap)
 static void *mem_alloc(const struct heap *pheap,size_t size)
 {
   void *p;
-  int   rc;
   
   ddt(pheap != NULL);
   ddt(size  > 0);
   ddt(size  < (130L*1024L));
 
-  do
-  {
-    rc = ERR_OKAY;
-    p  = malloc(size);
-    if (p == NULL)
-    {
-      ErrorPush(CgiErr,MEMALLOC,ERR_NOMEMORY,"");
-      rc = ErrThrow(&MemoryErr);
-    }
-  } while(rc == RETRY);
-
+  p = malloc(size);
+  if (p == NULL)
+    ddt(0);
+  
   return(p);
 }
 
 /*******************************************************************/
 
-static void *mem_grow(const struct heap *pheap,const void *p,size_t os,size_t ns)
-{
-  ddt(pheap != NULL);
-  ddt(p     != NULL);
-  ddt(os    >  0);
-  ddt(ns    >  0);
-  ddt(os    <  ns);
-
-  return(mem_resize(pheap,p,os,ns));
-}
-
-/**********************************************************************/
-
-void *mem_shrink(const struct heap *pheap,const void *p,size_t os,size_t ns)
-{
-  ddt(pheap != NULL);
-  ddt(p     != NULL);
-  ddt(os    >  0);
-  ddt(ns    >  0);
-  ddt(os    > ns);
-
-  return(mem_resize(pheap,p,os,ns));
-}
-
-/***********************************************************************/
-
-void *mem_resize(const struct heap *pheap,const void *p,size_t os,size_t ns)
+void *mem_resize(const struct heap *pheap,void *p,size_t ns)
 {
   void *np;
-  int   rc;
   
   ddt(pheap != NULL);
+  ddt(ns    >  0); 
   
-  do
-  {
-    rc = ERR_OKAY;
-    np = realloc((const Heap)p,ns);
-    if (np == NULL)
-    {
-      ErrorPush(CgiErr,MEMSHRINK,ERR_NOMEMORY,"");
-      rc = ErrThrow(&MemoryErr);
-    }
-  } while(rc == RETRY);
+  np = realloc(p,ns);
+  if (np == NULL)
+    ddt(0);
   
   return(np);
 }
 
 /************************************************************************/
 
-void mem_free(const struct heap *pheap,const void *p,size_t size)
+void mem_free(const struct heap *pheap,void *p)
 {
   ddt(pheap != NULL);
-  ddt(p     != NULL);
-  ddt(size  >  0);
-  
-  free((const Heap)p);
+
+  free(p);
 }
 
 /**************************************************************************/
