f6c532044e475fa4d1326b8eca6ae94ba942032d
Sean Conner <spc@conman.org>
2017-01-02 16:59:30 -0500

    Mark prototypes with extern.

--- src/dump.h
+++ src/dump.h
@@ -27,12 +27,12 @@
 
 #define DEF_DUMP_BYTES  16
 
-void hex            (char *,size_t,uintptr_t,size_t);
-int  hexdump_mems   (char *,size_t,const void *,size_t,size_t);
-int  chardump_mems  (char *,size_t,const void *,size_t,size_t);
-int  dump_mems      (char *,size_t,const void *,size_t,size_t,size_t);
-int  dump_memorys   (char *,size_t,const void *,size_t,size_t,size_t);
-int  dump_memoryf   (FILE *,       const void *,size_t,size_t,size_t);
-int  dump_memoryl   (int,          const void *,size_t,size_t,size_t);
+extern void hex           (char *,size_t,uintptr_t,size_t);
+extern int  hexdump_mems  (char *,size_t,const void *,size_t,size_t);
+extern int  chardump_mems (char *,size_t,const void *,size_t,size_t);
+extern int  dump_mems     (char *,size_t,const void *,size_t,size_t,size_t);
+extern int  dump_memorys  (char *,size_t,const void *,size_t,size_t,size_t);
+extern int  dump_memoryf  (FILE *,       const void *,size_t,size_t,size_t);
+extern int  dump_memoryl  (int,          const void *,size_t,size_t,size_t);
 
 #endif
